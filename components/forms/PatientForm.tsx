88dddf5214e443d5562492e6f15b158cc4fc281d
cyphix jonah <jonahyiga02@gmail.com>
2024-09-25 19:58:10 +0300

    Added an Emoji and a form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,8 +1,78 @@
-import React from 'react'
-
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Button } from "@/components/ui/button"
+import {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  Form,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  FormControl,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  FormDescription,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  FormField,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  FormItem,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  FormLabel,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  FormMessage,
+} from "@/components/ui/form"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Input } from "@/components/ui/input"
+ 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+ 
 const PatientForm = () => {
+  // 1. Define your form.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
   return (
-    <div> PatientForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+            <h1 className="header">Hi there 👋</h1>
+            <p className="tex-dark-700">Schedule your first appointment</p>
+        </section>
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
