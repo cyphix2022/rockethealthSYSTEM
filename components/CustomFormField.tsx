be061b08882cc13fe352b95e76a898f10bcb7b4e
cyphix jonah <jonahyiga02@gmail.com>
2024-09-25 21:00:09 +0300

    Making changes to the CustomFormField file

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -28,11 +28,11 @@ interface CustomProps{
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const CustomFormField = ({ control, fieldType }: CustomProps) => {
+const CustomFormField = ({ control, fieldType, name }: CustomProps) => {
   return (
     <FormField
           control={control}
-          name="username"
+          name={name}
           render={({ field }) => (
             <FormItem>
               <FormLabel>Username</FormLabel>
